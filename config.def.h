d95e2f4f66a92fdbc2c37463d2ecfa4143f376d3
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-05-23 19:38:25 +0800

    Add user & groups

--- config.def.h
+++ config.def.h
@@ -1,10 +1,10 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "bandithijo";
+static const char *group = "users";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INPUT] =  "#005f87",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
